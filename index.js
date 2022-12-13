1b6d465503c66be18abe81e839e40861f6c8a2b7
Maximilian Burger <burgermaxmicrosoft@gmail.com>
2022-12-13 20:49:19 +0100

    added background and refactored all files

--- index.js
+++ index.js
@@ -1,39 +1,31 @@
-function initBody() {
-    let main = document.getElementById('main');
-    for(let i = 2; i < main.children.length; i++) {
-        main.children[i].style.display = "none";
-    }
-}
-
-/*this function hides/shows divs, because only one html-file is allowed
-for grading (atleast that's what we think)*/
+/*this function hides/shows divs for onpage-navigation, because only one html-file is allowed for grading ;)*/
 function showChapter(chapterID) {
-   let main = document.getElementById('main');
-   for(let i = 1; i < main.children.length; i++) {
-      main.children[i].style.display = "none";
+   let mainContent = document.getElementById("mainContent");
+   for(let i = 1; i < mainContent.children.length; i++) {
+      mainContent.children[i].style.display = "none";
    }
 
    switch(chapterID){
       case 1:
-            main.children.dataBasedDocumentsChapter.style.display = "block";
+            mainContent.children.dataBasedDocumentsChapter.style.display = "block";
          break;
       case 2:
-            main.children.d3jsChapter.style.display = "block";
+            mainContent.children.d3jsChapter.style.display = "block";
          break;
       case 3:
-            main.children.marketChapter.style.display = "block";
+            mainContent.children.marketChapter.style.display = "block";
          break;
       case 4:
-            main.children.selfExamplesChapter.style.display = "block";
+            mainContent.children.selfExamplesChapter.style.display = "block";
          break;
       case 5:
-            main.children.publicExamplesChapter.style.display = "block";
+            mainContent.children.publicExamplesChapter.style.display = "block";
          break;
       case 6:
-            main.children.lessonsLearnedChapter.style.display = "block";
+            mainContent.children.lessonsLearnedChapter.style.display = "block";
          break;
       case 7:
-            main.children.aboutChapter.style.display = "block";
+            mainContent.children.aboutChapter.style.display = "block";
          break;
    }
 }
