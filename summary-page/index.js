802a11e668e4f1051f8386147eb266ad6b1b4395
MXBurger <burgermaxmicrosoft@gmail.com>
2023-01-28 13:24:49 +0100

    test scroll layout

--- summary-page/index.js
+++ summary-page/index.js
@@ -1,31 +1,33 @@
-/*this function hides/shows divs for onpage-navigation, because only one html-file is allowed for grading ;)*/
-function showChapter(chapterID) {
-   let mainContent = document.getElementById("mainContent");
-   for(let i = 1; i < mainContent.children.length; i++) {
-      mainContent.children[i].style.display = "none";
-   }
+// /*this function hides/shows divs for onpage-navigation, because only one html-file is allowed for grading ;)*/
+// function showChapter(chapterID) {
+//    let mainContent = document.getElementById("mainContent");
+//    for(let i = 1; i < mainContent.children.length; i++) {
+//       mainContent.children[i].style.display = "block";
+//    }
 
-   switch(chapterID){
-      case 1:
-            mainContent.children.dataBasedDocumentsChapter.style.display = "block";
-         break;
-      case 2:
-            mainContent.children.d3jsChapter.style.display = "flex";
-         break;
-      case 3:
-            mainContent.children.marketChapter.style.display = "block";
-         break;
-      case 4:
-            mainContent.children.selfExamplesChapter.style.display = "block";
-         break;
-      case 5:
-            mainContent.children.publicExamplesChapter.style.display = "block";
-         break;
-      case 6:
-            mainContent.children.lessonsLearnedChapter.style.display = "block";
-         break;
-      case 7:
-            mainContent.children.aboutChapter.style.display = "block";
-         break;
-   }
-}
+//    mainContent.children.d3jsChapter.style.display = "flex";
+
+//    switch(chapterID){
+//       case 1:
+//             mainContent.children.dataBasedDocumentsChapter.style.display = "block";
+//          break;
+//       case 2:
+//             mainContent.children.d3jsChapter.style.display = "flex";
+//          break;
+//       case 3:
+//             mainContent.children.marketChapter.style.display = "block";
+//          break;
+//       case 4:
+//             mainContent.children.selfExamplesChapter.style.display = "block";
+//          break;
+//       case 5:
+//             mainContent.children.publicExamplesChapter.style.display = "block";
+//          break;
+//       case 6:
+//             mainContent.children.lessonsLearnedChapter.style.display = "block";
+//          break;
+//       case 7:
+//             mainContent.children.aboutChapter.style.display = "block";
+//          break;
+//    }
+// }
