71d6440aa0ce2a44d5cd4297811da90ba9afa516
MXBurger <burgermaxmicrosoft@gmail.com>
2023-01-29 18:51:59 +0100

    more work on summary page

--- submission/Piechart/piechart.js
+++ submission/Piechart/piechart.js
@@ -1,32 +1,32 @@
-let data = [
+let pieChartData = [
     [0,  20, 30, 50],
     [10,  220, 60, 510],
     [21312, 1239, 1242, 9341]
  ];
  
-let margin = 10;
-let radius = 100;
+let ma = 10;
+let rad = 100;
 
 //maps color to data-values
 let color = d3.scaleOrdinal()
-  .domain(data)
+  .domain(pieChartData)
   .range(["#66c2a5","#fc8d62","#8da0cb","#e78ac3","#a6d854","#ffd92f","#e5c494","#b3b3b3"]);
 
 //append a svg to body and center the coordinate system via translate
 let svg = d3.select("body").selectAll("svg")
-  .data(data)
+  .data(pieChartData)
   .enter().append("svg")
-  .attr("width", (radius + margin) * 2)
-  .attr("height", (radius + margin) * 2)
+  .attr("width", (rad + ma) * 2)
+  .attr("height", (rad + ma) * 2)
   .append("g")
-  .attr("transform", "translate(" + (radius + margin) + "," + (radius + margin) + ")");
+  .attr("transform", "translate(" + (rad + ma) + "," + (rad + ma) + ")");
  
 //use the d3 pie layout to draw path(s)
 svg.selectAll("path")
   .data(d3.pie())
   .enter().append("path")
   .attr("d", d3.arc()
-  .innerRadius(radius * 0.5)
-  .outerRadius(radius))
+  .innerRadius(rad * 0.5)
+  .outerRadius(rad))
   .style("fill", (d) => (color(d.data)))
   .style("stroke", "black");
--- summary-page/index.js
+++ summary-page/index.js
@@ -1,33 +1,11 @@
-// /*this function hides/shows divs for onpage-navigation, because only one html-file is allowed for grading ;)*/
-// function showChapter(chapterID) {
-//    let mainContent = document.getElementById("mainContent");
-//    for(let i = 1; i < mainContent.children.length; i++) {
-//       mainContent.children[i].style.display = "block";
-//    }
+//function to make the selected chapters appear bold in the nav menu
+let currElem = null; //will hold the element that is bold now
 
-//    mainContent.children.d3jsChapter.style.display = "flex";
-
-//    switch(chapterID){
-//       case 1:
-//             mainContent.children.dataBasedDocumentsChapter.style.display = "block";
-//          break;
-//       case 2:
-//             mainContent.children.d3jsChapter.style.display = "flex";
-//          break;
-//       case 3:
-//             mainContent.children.marketChapter.style.display = "block";
-//          break;
-//       case 4:
-//             mainContent.children.selfExamplesChapter.style.display = "block";
-//          break;
-//       case 5:
-//             mainContent.children.publicExamplesChapter.style.display = "block";
-//          break;
-//       case 6:
-//             mainContent.children.lessonsLearnedChapter.style.display = "block";
-//          break;
-//       case 7:
-//             mainContent.children.aboutChapter.style.display = "block";
-//          break;
-//    }
-// }
+ function BoldText(elem) {
+  if (elem != currElem) { //do thing if you're clicking on a bold link
+   if (currElem) //if a link bold right now unbold it
+	currElem.style.fontWeight='normal';
+   currElem = elem; //assign this element as the current one
+   elem.style.fontWeight='bold';  //make the element clicked bold
+  }
+ }
